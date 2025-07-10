f06aa052c173184723a7e5a074cacea2a92d2c92
Nicki-28 <nickiissj@gmail.com>
2025-07-11 00:31:09 +0200

    flask server frontEnd

--- src/chatBot.py
+++ src/chatBot.py
@@ -4,6 +4,8 @@ import json
 from config import API_KEY
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import re
+
 
 app = Flask(__name__)
 CORS(app)
@@ -21,7 +23,7 @@ def obtener_respuesta(query_str):
     messages = [
         {
             "role": "system",
-            "content": "Eres un asistente financiero llamado Finn. Analiza los gastos y da consejos de ahorro. Presentate primero."
+            "content": "Eres un asistente financiero llamado Finn. Analiza los gastos y da consejos de ahorro."
         },
         {
             "role": "user",
@@ -41,11 +43,15 @@ def obtener_respuesta(query_str):
        
         #print(f"Código de respuesta: {response.status_code}")
         #print("Contenido de la respuesta:", response.text)  
-
+        
         if response.status_code == 200:
-            result = response.json()
-            print("Análisis Financiero:")
-            print(result['choices'][0]['message']['content'])  # Acceder al contenido de la respuesta
+            parcialResult = response.json()
+            #print("Análisis Financiero:")
+            #print(result['choices'][0]['message']['content']) 
+            parcialResult1 = parcialResult['choices'][0]['message']['content']  # Acceder al contenido de la respuesta
+            parcialResult2 = re.sub(r"(?<=\w)\d+\b", "", parcialResult1)# Eliminar números pegados a letras
+            result= re.sub(r"[\[\]\*]", "", parcialResult2) # Eliminar caracteres especiales: [, ], *
+ 
         else:
             print("Error al llamar a la API:", response.status_code)
             print(response.json())
@@ -54,17 +60,19 @@ def obtener_respuesta(query_str):
         print("Ha ocurrido un error, por el momento no puedo ayudarte. Inténtalo más tarde", e)
     except Exception as e:
         print("Por el momento no tengo consejos financieros para ti", e)
+    
+    return result
 
 
-@app.route('/api/chat', methods=['POST'])
+@app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
-    query_str = data.get('query', '')
+    query_str = data.get('query', '') #obtenemos la peticioón
 
     if not query_str:
         return jsonify({"error": "Consulta no proporcionada"}), 400
 
-    respuesta = obtener_respuesta(query_str) 
+    respuesta = obtener_respuesta(query_str) #llamamos al bot
     return jsonify({"reply": respuesta})
 
 if __name__ == '__main__':
